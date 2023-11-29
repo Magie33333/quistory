51e85a2021358851b53d5b7b4a1dc29d9f0cc7ff
Magie33333 <146265892+Magie33333@users.noreply.github.com>
2023-11-29 23:41:47 +0100

    Delete KvizVyber.php

--- KvizVyber.php
+++
@@ -1,6 +0,0 @@
-<?php
-include 'KvizController.php';
-$controller = new KvizController($conn);
-$kvizy = $controller->zobrazKvizy(); // Předpokládá se, že tato metoda existuje a získává data kvízů
-include 'KvizVyberView.php'; // Předá data kvízů do view
-?>
